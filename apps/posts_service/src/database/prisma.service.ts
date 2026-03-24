4c89043831a056009c172c94789fe6c14a008954
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-03-24 13:14:53 +0700

    fix prisma 7

--- apps/posts_service/src/database/prisma.service.ts
+++ apps/posts_service/src/database/prisma.service.ts
@@ -1,8 +1,19 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor(config: ConfigService) {
+        super({
+            datasources: {
+                db: {
+                    url: config.getOrThrow<string>('DATABASE_URL'),
+                },
+            },
+        });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
