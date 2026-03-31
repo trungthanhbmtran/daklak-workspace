fdc8b77f28745f1c197440a6ba450c21dc6123a6
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-03-31 22:14:30 +0700

    fix

--- apps/posts_service/prisma.config.ts
+++ apps/posts_service/prisma.config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig({
-    schema: "prisma/schema.prisma",
+    schema: "prisma",
     migrations: {
         path: "prisma/migrations",
         seed: "prisma/seed.ts",
