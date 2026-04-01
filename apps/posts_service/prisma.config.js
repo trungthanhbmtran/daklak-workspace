b8f1df4c5badfc2639defc09bce907f8dd2d925f
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-04-01 12:15:06 +0700

    fix

--- apps/posts_service/prisma.config.js
+++ apps/posts_service/prisma.config.js
@@ -1,14 +1,10 @@
 const { defineConfig } = require("prisma/config");
 require("dotenv").config();
 
-console.log(process.env.DATABASE_URL);
 module.exports = defineConfig({
   schema: "prisma",
   migrations: {
     path: "prisma/migrations",
     seed: "prisma/seed.ts",
-  },
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
+  }
 });
