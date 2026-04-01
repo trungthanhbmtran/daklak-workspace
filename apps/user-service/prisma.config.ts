7b4056cec829474eaa162c04561713ee4c0cd633
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-04-01 19:20:57 +0700

    d

--- apps/user-service/prisma.config.ts
+++ apps/user-service/prisma.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env.DATABASE_URL || "mysql://root:password@localhost:3306/db",
+    url: process.env.DATABASE_URL,
   },
 });
