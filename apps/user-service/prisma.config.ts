b5fda7c4bc2927176c31fd91fff220597decc21a
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-04-01 17:12:58 +0700

    d

--- apps/user-service/prisma.config.ts
+++ apps/user-service/prisma.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema",
+  schema: "schema",
   migrations: {
     path: "prisma/migrations",
     seed: "tsx prisma/seed.ts",
