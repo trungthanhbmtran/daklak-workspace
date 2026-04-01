900a66d7f3e8a1d67591103b472cea4a095b421a
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-04-01 15:47:18 +0700

    up

--- apps/posts_service/prisma.config.ts
+++ apps/posts_service/prisma.config.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import type { PrismaConfig } from "prisma";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("❌ DATABASE_URL is missing");
+}
+
 export default {
   schema: "prisma/schema.prisma",
 
