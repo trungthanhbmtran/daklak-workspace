3c6a29b56d94706292fc23a0802a927d1171d879
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-04-07 14:38:36 +0700

    update

--- apps/media-service/src/app.module.ts
+++ apps/media-service/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@/config/config.module';
 import { StorageModule } from '@/modules/storage/storage.module';
+import { PrismaModule } from '@/database/prisma.module';
 
 
 @Module({
   imports: [
     ConfigModule,
+    PrismaModule,
     StorageModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
