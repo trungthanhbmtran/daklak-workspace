8494a6e8c40c61a7f37c040498e3877c60d015cf
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-04-23 07:37:00 +0700

    d

--- apps/posts-service/src/modules/posts/posts.module.ts
+++ apps/posts-service/src/modules/posts/posts.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { WorkflowModule } from '../workflow/workflow.module';
+
 
 @Module({
+  imports: [WorkflowModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
+
 export class PostsModule {}
