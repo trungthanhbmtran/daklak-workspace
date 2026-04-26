32ffed21d36b78b32d434b4ffcaf985aeafd7a71
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-04-26 12:11:48 +0700

    d

--- apps/document-service/src/modules/category/category.service.ts
+++ apps/document-service/src/modules/category/category.service.ts
@@ -3,7 +3,7 @@ import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class CategoryService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async create(data: any) {
     const slug = data.slug || this.generateSlug(data.name);
