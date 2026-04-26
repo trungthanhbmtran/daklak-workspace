70fb0336819e92cadd0db2e6e0417e19a497e737
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-04-26 11:08:51 +0700

    d

--- apps/document-service/src/main.ts
+++ apps/document-service/src/main.ts
@@ -4,13 +4,16 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const protoRoot = process.env.PROTO_PATH ?? join(__dirname, '../../../shared/protos');
+  const docDir = join(protoRoot, 'document');
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.GRPC,
     options: {
       package: ['document', 'category'],
       protoPath: [
-        join(__dirname, '../../../shared/protos/document/document.proto'),
-        join(__dirname, '../../../shared/protos/document/category.proto'),
+        join(docDir, 'document.proto'),
+        join(docDir, 'category.proto'),
       ],
       url: '0.0.0.0:50056',
     },
