62029bc1a9878ec91aecefec56e895e2ee211ae4
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-05-01 18:41:28 +0700

    d

--- apps/admin_khcn/features/workflow/api.ts
+++ apps/admin_khcn/features/workflow/api.ts
@@ -43,32 +43,32 @@ function unwrapMeta(res: any): any {
 
 export const workflowApi = {
   list: (params: { skip?: number; take?: number } = {}) =>
-    apiClient.get("/admin/workflow", { params }).then((res: any) => ({
+    apiClient.get("/workflow", { params }).then((res: any) => ({
       data: unwrapData<Workflow[]>(res),
       meta: unwrapMeta(res),
     })),
 
   getOne: (id: string) =>
-    apiClient.get(`/admin/workflow/${id}`).then((res: any) => unwrapData<Workflow>(res)),
+    apiClient.get(`/workflow/${id}`).then((res: any) => unwrapData<Workflow>(res)),
 
   create: (data: Partial<Workflow>) =>
-    apiClient.post("/admin/workflow", data).then((res: any) => unwrapData<Workflow>(res)),
+    apiClient.post("/workflow", data).then((res: any) => unwrapData<Workflow>(res)),
 
   update: (id: string, data: Partial<Workflow>) =>
-    apiClient.put(`/admin/workflow/${id}`, data).then((res: any) => unwrapData<Workflow>(res)),
+    apiClient.put(`/workflow/${id}`, data).then((res: any) => unwrapData<Workflow>(res)),
 
   delete: (id: string) =>
-    apiClient.delete(`/admin/workflow/${id}`).then((res: any) => unwrapData<any>(res)),
+    apiClient.delete(`/workflow/${id}`).then((res: any) => unwrapData<any>(res)),
 
   start: (id: string, initialContext: any = {}) =>
-    apiClient.post(`/admin/workflow/${id}/start`, { initialContext }).then((res: any) => unwrapData<WorkflowInstance>(res)),
+    apiClient.post(`/workflow/${id}/start`, { initialContext }).then((res: any) => unwrapData<WorkflowInstance>(res)),
 
   resume: (instanceId: string, nodeId: string, actionData: any = {}) =>
-    apiClient.post(`/admin/workflow/instances/${instanceId}/resume/${nodeId}`, { actionData }).then((res: any) => unwrapData<WorkflowInstance>(res)),
+    apiClient.post(`/workflow/instances/${instanceId}/resume/${nodeId}`, { actionData }).then((res: any) => unwrapData<WorkflowInstance>(res)),
 
   getInstance: (id: string) =>
-    apiClient.get(`/admin/workflow/instances/${id}`).then((res: any) => unwrapData<WorkflowInstance>(res)),
+    apiClient.get(`/workflow/instances/${id}`).then((res: any) => unwrapData<WorkflowInstance>(res)),
 
   getLogs: (instanceId: string) =>
-    apiClient.get(`/admin/workflow/instances/${instanceId}/logs`).then((res: any) => unwrapData<any[]>(res)),
+    apiClient.get(`/workflow/instances/${instanceId}/logs`).then((res: any) => unwrapData<any[]>(res)),
 };
