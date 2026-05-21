5a383496e7500ab17da47d023a9cbabef69b6f41
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-05-21 18:42:11 +0700

    d'd

--- apps/admin_khcn/app/services/hrm/departments/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DepartmentListClient } from "@/features/hrm";
-
-export const metadata = {
-  title: "Sơ đồ tổ chức | Quản lý Nhân sự",
-};
-
-export default function DepartmentsPage() {
-  return <DepartmentListClient />;
-}
