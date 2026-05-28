147e0900b07b6c115143b03440b2473f9c397c96
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-05-28 21:07:32 +0700

    d

--- apps/admin_khcn/app/services/hrm/work-plans/master-plans/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { PlanDetailClient } from "@/features/hrm/components/plans/PlanDetailClient";
-
-export default async function PlanDetailPage({ params }: { params: Promise<{ id: string }> }) {
-  const resolvedParams = await params;
-  return <PlanDetailClient planId={Number(resolvedParams.id)} />;
-}
