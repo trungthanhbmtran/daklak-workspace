c2ee328c50717105685c74b960efb8839be5c86b
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-06-03 18:55:48 +0700

    d

--- apps/admin_khcn/app/services/hrm/staffing/page.tsx
+++
@@ -1,5 +0,0 @@
-import { StaffingDashboardClient } from '@/features/hrm/components/staffing/StaffingDashboardClient';
-
-export default function StaffingPage() {
-  return <StaffingDashboardClient />;
-}
