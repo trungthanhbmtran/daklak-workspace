e8e7bcfd003145972b78fcaf25bbc166e3dbaed4
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-06-20 17:41:20 +0700

    D

--- apps/admin_khcn/app/services/hrm/work-plans/manual-selector/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ManualPlanSelectorByRankClient } from '@/features/hrm/components/work-plans/master-plans/ManualPlanSelectorByRankClient';
-
-export default function ManualSelectorPage() {
-  return <ManualPlanSelectorByRankClient />;
-}
