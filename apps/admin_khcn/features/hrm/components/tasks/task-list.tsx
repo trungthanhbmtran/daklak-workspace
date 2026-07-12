37da8046f8b4c808a5598797c54c6c839c83b8c3
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-07-12 21:18:56 +0700

    d

--- apps/admin_khcn/features/hrm/components/tasks/task-list.tsx
+++ apps/admin_khcn/features/hrm/components/tasks/task-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
