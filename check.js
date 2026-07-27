df66f4f8e5bb80063d558fb023a03cfe182ecea0
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-07-27 11:43:41 +0700

    d

--- check.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const apps = fs.readdirSync('apps');
-apps.forEach(app => {
-  const hasConfig = fs.existsSync(`apps/${app}/prisma.config.ts`);
-  const hasSchemaFile = fs.existsSync(`apps/${app}/prisma/schema.prisma`);
-  const hasSchemaDir = fs.existsSync(`apps/${app}/prisma/schema`);
-  console.log(`${app}: config=${hasConfig}, schema.prisma=${hasSchemaFile}, schema_dir=${hasSchemaDir}`);
-});
