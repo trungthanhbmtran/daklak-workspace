d2977b834a26b7c3db589b3526723c0f0c9cc05c
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-07-28 21:56:40 +0700

    D

--- install_adapters.ps1
+++
@@ -1,9 +0,0 @@
-$apps = Get-ChildItem -Path apps -Directory
-foreach ($app in $apps) {
-  if (Test-Path "$($app.FullName)/prisma") {
-    Write-Host "Installing in $($app.Name)..."
-    Push-Location "$($app.FullName)"
-    npm install @prisma/adapter-mariadb mariadb
-    Pop-Location
-  }
-}
