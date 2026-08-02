fe13b80091b90e2607fea77719cb081e05bdb4f1
trungthanhbmt.tran@gmail.com <trungthanhbmt.tran@gmail.com>
2026-08-02 21:55:54 +0700

    feat: add missing notifications config page to fix 404

---
+++ apps/admin_khcn/app/hub/notifications/config/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NotificationConfigPanel } from '@/features/notifications/components/NotificationConfigPanel';
+
+export default function NotificationConfigPage() {
+  return (
+    <div className="p-6">
+      <NotificationConfigPanel />
+    </div>
+  );
+}
